796d9edcd08838fcf99fb83aba0872968d529e94
Alan Williams <theydonthaveit@gmail.com>
2018-10-24 16:14:54 +0100

    returns confidence scores for each language required

--- find_the_best.py
+++ find_the_best.py
@@ -11,76 +11,78 @@ with open('phrases.txt', 'r') as f:
         phrases.append(row.rstrip('\n'))
 
 audio_data = ''
-with open('14_36_01.log') as f:
+with open('15_59_53.log') as f:
     audio_data = f.read()
 
 
-# def classify_language_transcription(languages: list) -> list:
-#     confidence_percentage_for_each_language = []
+def classify_language_transcription(languages: list, confidence_percentage_for_each_language: list) -> list:
 
-#     if (languages.__len__() == 0):
-#         return confidence_percentage_for_each_language
+    if (languages.__len__() == 0):
+        return confidence_percentage_for_each_language
 
-#     language_with_confidence_level = request_for_transcription(languages.pop(0))
-#     confidence_percentage_for_each_language.append(
-#         language_with_confidence_level
-#     )
+    language_with_confidence_level = request_for_transcription(languages.pop(0))
 
-#     return classify_language_transcription(languages)
+    confidence_percentage_for_each_language.append(
+        language_with_confidence_level
+    )
+
+    return classify_language_transcription(languages, confidence_percentage_for_each_language)
 
 
 def request_for_transcription(language: str) -> dict:
-    result =\
-    requests.post(f'https://speech.googleapis.com/v1/speech:recognize?key={key}',
-    headers={
-        "Content-Type": "application/json"
-    },
-    data=json.dumps({
-        "config": {
-            "encoding": "FLAC",
-            "sampleRateHertz": 44100,
-            "languageCode": language,
-            "speechContexts": [
-                {
-                    "phrases": phrases
-                }
-            ],
-            "maxAlternatives": 10
+    result = requests.\
+        post(f'https://speech.googleapis.com/v1/speech:recognize?key={key}',
+        headers={
+            "Content-Type": "application/json"
         },
-        "audio": {
-            "content": audio_data
-        }
-    }))
+        data=json.dumps({
+            "config": {
+                "encoding": "FLAC",
+                "sampleRateHertz": 44100,
+                "languageCode": language,
+                "speechContexts": [
+                    {
+                        "phrases": phrases
+                    }
+                ],
+                "maxAlternatives": 10
+            },
+            "audio": {
+                "content": audio_data
+            }
+        }))
 
-    retrieve_confidence_with_transcription(result.json())
-    # return json.loads(result)
+    return retrieve_confidence_with_transcription(result.json())
 
 
 def retrieve_confidence_with_transcription(transcription_result: dict) -> dict:
     all_transcription_possible =\
-        json.dumps(transcription_result["results"][0]["alternatives"])
+        transcription_result["results"][0]["alternatives"]
 
-    retrieve_transcription_with_highest_confidence(
-        all_transcription_possible
+    return retrieve_transcription_with_highest_confidence(
+        all_transcription_possible, {"confidence": 0}, {"confidence": 0}
     )
 
 
 def retrieve_transcription_with_highest_confidence(
     all_transcriptions: list,
-    previous_transcript: dict ) -> dict:
-
-    transcription_with_highest_confidence = {}
-    current_highest_confidence_level = previous_transcript["confidence"]
+    previous_transcript: dict,
+    transcription_with_highest_confidence: dict ) -> dict:
 
     if (all_transcriptions.__len__() == 0):
         return transcription_with_highest_confidence
 
     current_transcript = all_transcriptions.pop(0)
 
-    if (current_highest_confidence_level < current_transcript["confidence"]):
-        transcription_with_highest_confidence = current_transcript
+    if (current_transcript["confidence"] > previous_transcript["confidence"]\
+        and current_transcript["confidence"] > transcription_with_highest_confidence["confidence"]):
+            transcription_with_highest_confidence = current_transcript
 
-request_for_transcription('en-GB')
-# if __name__ == '__main__':
-#     langauges = ['en-GB', 'en-SA', 'en-US']
-#     classify_language_transcription(langauges)
+    return retrieve_transcription_with_highest_confidence(
+        all_transcriptions, current_transcript, transcription_with_highest_confidence )
+
+
+if __name__ == '__main__':
+    langauges = ['en-GB', 'en-ZA', 'en-US']
+    c = classify_language_transcription(langauges, [])
+    print(c)
